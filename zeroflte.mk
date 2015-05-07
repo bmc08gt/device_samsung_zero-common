76dddbf6ac65537a0e9fb311585ddce7b0824d3d
bmc08gt <brandon.mcansh@gmail.com>
2015-05-07 15:18:39 +0200

    zeroflte: Add Google codecs
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- zeroflte.mk
+++ zeroflte.mk
@@ -26,7 +26,10 @@ PRODUCT_COPY_FILES += \
 # Media profile
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Misc
 PRODUCT_PACKAGES += \
