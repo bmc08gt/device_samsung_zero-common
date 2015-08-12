9555fc0f73c6d19edd472d43ade8516c27f97e7b
bmc08gt <brandon.mcansh@gmail.com>
2015-08-12 22:47:27 +0200

    Move mkbootimg.mk to common and simplify it
    * dtbTool creates te dtb list, so there is no need to duplicate that step
    
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,7 @@ BOARD_USES_HWCOMPOSER := true
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7420
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --second_offset 0x10f00000 --tags_offset 0x00000100
